32b21ab088a2b5c0e390affbe088a57b7c5cf059
Amina Hamoud <amina123@tt.com>
2025-08-27 15:15:57 +0300

    feat:Improvements in the model files

--- app/Core/Response.php
+++ app/Core/Response.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Core;
-require_once __DIR__ . '/Response.php';
+// require_once __DIR__ . '/Response.php';
 
 class Response {
   public function json($data, int $code=200): void {
